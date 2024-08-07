cebcffa1d3284bbc558a0227017de0cc74bccea2
Sinan Turan <turansinan2004@gmail.com>
2024-08-07 12:15:26 +0300

    Proje dosyası ekle.

---
+++ WebAPI/Repositories/RepositoryContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.Repositories
+{
+    public class RepositoryContext: DbContext
+    {
+        public DbSet<Book>Books { get; set; }
+    }
+}
