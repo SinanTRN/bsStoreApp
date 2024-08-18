a12ab68bc29a2055f937271983bda426189166cd
Sinan Turan <turansinan2004@gmail.com>
2024-08-18 10:10:52 +0300

    AutoMapper used

--- Entities/DataTransferObjects/BookDtoForUpdate.cs
+++ Entities/DataTransferObjects/BookDtoForUpdate.cs
@@ -6,5 +6,5 @@ using System.Threading.Tasks;
 
 namespace Entities.DataTransferObjects
 {
-    public record BookDtoForUpdate(int Id,string Title,decimal Price);
+    public record BookDtoForUpdate(int Id,String Title,decimal Price);
 }
