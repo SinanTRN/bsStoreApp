a54368dde004e9b4e87a4a54666b995814bb90b7
Sinan Turan <turansinan2004@gmail.com>
2024-08-19 17:11:17 +0300

    Added a class named PagedList

---
+++ Entities/RequestFeatures/MetaData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.RequestFeatures
+{
+    public class MetaData
+    {
+        public int CurrentPage { get; set; }
+        public int TotalPage { get; set; }
+        public int PageCount { get; set; }
+        public int TotalCount { get; set; }
+
+        public bool HasPrevious => CurrentPage > 1;
+        public bool HasNext => CurrentPage < TotalPage;
+    }
+}
